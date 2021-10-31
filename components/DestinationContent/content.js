281513699bbc0621ec13d547eb2f48da96e9278a
Tomas Roaldsnes <tomas.roaldsnes@schibsted.com>
2021-10-31 20:31:30 +0100

    oops replace not replaceAll

--- components/DestinationContent/content.js
+++ components/DestinationContent/content.js
@@ -6,7 +6,7 @@ import InstagramEmbed from 'react-instagram-embed';
 //import AdSense from 'react-adsense';
 
 export default function DestinationContent({ destination }) {
-  const tag = destination.tags.replaceAll('|', '·');
+  const tag = destination.tags.replace('|', '·');
   return (
     <div className={content.container}>
       <h2 className={content.heading}>{destination.title}</h2>
