c4fd49c835a2451447ffc04496fe372f2d8da3ee
Tomas Roaldsnes <tomas.roaldsnes@schibsted.com>
2022-08-25 22:49:58 +0200

    menu

--- components/Nav/menu.js
+++ components/Nav/menu.js
@@ -1,31 +1,42 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import layout from '../../styles/Layout.module.scss'
-import button from '../Buttons.module.scss'
+import Image from "next/image";
+import Link from "next/link";
+import layout from "../../styles/Layout.module.scss";
+import button from "../Buttons.module.scss";
 
 export default function Nav() {
   return (
     <div className={!open ? layout.menu : layout.menuOpen}>
       <ul className={layout.menuList}>
         <li className={layout.menuItem}>
-        <Link href="/destinations">
-          <a>Destinasjoner</a>
+          <Link href="/destinasjoner">
+            <a>Destinasjoner</a>
           </Link>
         </li>
         <li className={layout.menuItem}>
-        <Link href="/destinations?keyword=Parkering">
-          <a>Steder med parkering</a>
-        </Link>
+          <Link href="/destinasjoner?keyword=Familie">
+            <a>For familien</a>
+          </Link>
         </li>
         <li className={layout.menuItem}>
-        <Link href="/destinations?keyword=Langtur">
-          <a>Langturer</a>
-        </Link>
+          <Link href="/destinasjoner?keyword=Topptur">
+            <a>Toppturer</a>
+          </Link>
+        </li>
+
+        <li className={layout.menuItem}>
+          <Link href="/destinasjoner?keyword=Sopptur">
+            <a>Soppturer</a>
+          </Link>
         </li>
         <li className={layout.menuItem}>
-        <Link href="/destinations?keyword=Familie">
-          <a>For familien</a>
-        </Link>
+          <Link href="/destinasjoner?keyword=Sykkel">
+            <a>Sykkelturer</a>
+          </Link>
+        </li>
+        <li className={layout.menuItem}>
+          <Link href="/destinasjoner?keyword=Kajakk">
+            <a>Kano og kajakk</a>
+          </Link>
         </li>
         {/* <li className={layout.menuItemNoHover}>
           <div className={button.containerRow}>
@@ -35,6 +46,5 @@ export default function Nav() {
         </li> */}
       </ul>
     </div>
-  )
+  );
 }
-
